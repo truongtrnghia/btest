cd5be39ce8df8aaaa8af347819eb98171bdc6079
Nghia <truongtrnghia@gmail.com>
2021-03-01 11:49:41 +0700

    init commit

--- publish_amie.sh
+++ publish_amie.sh
@@ -2,12 +2,16 @@
 
 if [ -z "$1" ]; then
   echo 'Usage: publish_amie [major|minor|patch]'
+  echo 'Must have at least one commit from the last publish'
   exit 1
 fi
-
+lerna -h 1&2> /dev/null
+if [ "$?" != "0" ]; then
+  echo 'Install lerna by: npm i -g lerna'
+fi
 npm whoami 2> /dev/null
-if [ "$?" == "1" ]; then
-  echo Login npm first
+if [ "$?" != "0" ]; then
+  echo 'Login npm first'
   npm login
 else
   echo 'Logged in '
