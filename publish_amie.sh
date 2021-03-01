dc120d595334c83e890146a236379a20d122e383
Nghia <truongtrnghia@gmail.com>
2021-03-01 13:13:13 +0700

    init commit

--- publish_amie.sh
+++ publish_amie.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+registry="http://localhost:4873"
 if [ -z "$1" ]; then
   echo 'Usage: publish_amie [major|minor|patch]'
   echo 'Must have at least one commit from the last publish'
@@ -9,12 +9,12 @@ lerna -h 2>&1 > /dev/null
 if [ "$?" != "0" ]; then
   echo 'Install lerna by: npm i -g lerna'
 fi
-npm whoami 2> /dev/null
+npm whoami --registry "$registry" 2> /dev/null
 if [ "$?" != "0" ]; then
   echo 'Login npm first'
-  npm login
+  npm login --registry "$registry"
 else
   echo 'Logged in '
 fi
 lerna version $1
-lerna publish from-git
+lerna publish from-git --registry "$registry"
